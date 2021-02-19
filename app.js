726ee3daa5f3fa5f9d1f0f84fb715c448e37ce58
tylerdahl123 <dahlgren15@gmail.com>
2021-02-18 17:44:04 -0800

    managed to finish the app thingy

--- app.js
+++ app.js
@@ -3,42 +3,69 @@ const team = {
       {
         firstName: 'Tom',
         lastName: 'Brady',
-        age: '43'
+        age: 43
        },
 
        {
         firstName: 'Rob',
         lastName: 'Gronkowski',
-         age: '31',
+         age: 31,
        },
 
        {
            firstName: 'Tyler',
            lastName: 'Dahlgren',
-           age: '25',
+           age: 25,
        }
     ],
     _games:[
         {
-            opponent: 'Kansas City',
+            opponentsName: 'Kansas City',
             teamPoints: 21,
             opponentPoints: 3,
         },
         {
-            opponent: 'Washington Football Team',
+            opponentsName: 'Washington Football Team',
             teamPoints: 69,
-            opponent: 21,
+            opponentPoints: 21,
         },
         {
-            opponent: 'LA Rams',
+            opponentsName: 'LA Rams',
             teamPoints: 420,
-            opponent: 69,
+            opponentPoints: 69,
         }
     ],
-    get _players(){
+    get players(){
         return this._players;
     },
-    get _games(){
+    get games(){
         return this._games;
+    },
+    addPlayer (firstName, lastName, age) {
+        let player = {
+            firstName: firstName,
+            lastName: lastName,
+            age: age
+        }
+        this.players.push(player)
+    },
+    addGame (opponentsName, teamPoints, opponentPoints){
+            let game = {
+                opponentsName: opponentsName,
+                teamPoints: teamPoints,
+                opponentPoints: opponentPoints
+            }
+            this.games.push(game);
     }
   };
+
+  team.addPlayer('Steph', 'Curry', 28);
+  team.addPlayer('Lisa', 'Leslie', 44);
+  team.addPlayer('Bugs', 'Bunny', 76);
+  team.addGame('Broncos', 45, 21);
+  team.addGame('Steelers', 69, 420);
+
+
+
+  console.log(team._players);
+  console.log(team._games);
